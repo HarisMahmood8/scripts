334364c9062cb931ab3097d73718dec263043529
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-05 15:44:05 -0400

    Update duplct.py

--- duplct.py
+++ duplct.py
@@ -1,33 +1,63 @@
-from Reading_files import generate_full
 import pandas as pd
+from Reading_files import test_case_generator, cleaning_up_special_characters, get_cases, find_new_path
 
-def remove_duplicates(all_test_cases, precedence):
-    unique_test_cases = []
+def find_duplicates(test_cases):
+
+    # Finds duplicate test cases based on their name.
+    # Returns a list of unique test cases, considering precedence.
     
-    for test_case in all_test_cases:
-        duplicate_case = False
-        for unique_case in unique_test_cases:
-            if test_case.case_data == unique_case.case_data:
-                prior_environment_index = precedence.index(unique_case.environment)
-                current_environment_index = precedence.index(test_case.environment)
-                
-                if current_environment_index < prior_environment_index:
-                    unique_test_cases.remove(unique_case)
-                    unique_test_cases.append(test_case)
-                    
-                duplicate_case = True
+    unique_cases = []
+    duplicates = []
+
+    for case in test_cases:
+        if case.name not in [c.name for c in unique_cases]:
+            unique_cases.append(case)
+        else:
+            duplicates.append(case)
+
+    for duplicate in duplicates:
+        for index, unique_case in enumerate(unique_cases):
+            if duplicate.name == unique_case.name:
+                if duplicate.environment in unique_case.environment:
+                    # Duplicate has higher precedence, replace the existing case
+                    unique_cases[index] = duplicate
                 break
-        if not duplicate_case:
-            unique_test_cases.append(test_case)
-            
-    return unique_test_cases 
-    
-if(__name__== "__main__"):
-    
+
+    return duplicates
+
+def generate_duplicates_dataframe(duplicates):
+    data = {
+        "Environment": [],
+        "File Name": [],
+        "Data Similarity": []
+    }
+
+    unique_names = set()  # Track unique names
+
+    for duplicate in duplicates:
+        if duplicate.name in unique_names:
+            data["Environment"].append(duplicate.environment)
+            data["File Name"].append(duplicate.new_path)
+            data["Data Similarity"].append("Duplicate")
+        else:
+            unique_names.add(duplicate.name)
+
+    df_duplicates = pd.DataFrame(data)
+    return df_duplicates
+
+if __name__ == "__main__":
     TEST_DATA_DIRECTORY = "test-data"
-    Test_Cases, df = generate_full(TEST_DATA_DIRECTORY)
-        
-    precedence = ["insprint", "system", "insprinttt", "systemtt", "uat", "december"]
-    unique_cases = remove_duplicates(Test_Cases,precedence)
-    
-    print(f"Number of Unique Test Cases:{len(unique_cases)}")
+    file_paths = get_cases(TEST_DATA_DIRECTORY)
+
+    test_cases = []
+    for i in file_paths:
+        input_data = i
+        new_folder = find_new_path(input_data)
+        cases, _ = test_case_generator(input_data, new_folder)
+        test_cases += cases
+
+    duplicate_cases = find_duplicates(test_cases)
+
+    df_duplicates = generate_duplicates_dataframe(duplicate_cases)
+
+    df_duplicates.to_csv("Duplicates_Report.csv", index=False)

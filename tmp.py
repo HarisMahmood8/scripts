1b1647b0613f25aa7c58921a407fd08ec9dfabb2
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-11 14:10:39 -0400

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,24 +1,17 @@
 def cleaning_up_special_characters(name):
     error = None
     max_length = 255
-    special_char_removed_name = ''.join(c for c in name if c.isalnum() or c == '_')
-    shortened_name = special_char_removed_name.replace(" ", "_")[:max_length]
-    if len(name) != len(shortened_name):
-        error = "Maximum characters exceeded"
-    elif '_' not in shortened_name:
-        error = "Special characters found"
-    return shortened_name, error
-
-
-def cleaning_up_special_characters(name):
-    error = None
-    max_length = 255
     cleaned_name = name.replace(" ", "_")
     special_char_removed_name = ''.join(c for c in cleaned_name if c.isalnum() or c == '_')
     shortened_name = special_char_removed_name[:max_length]
-    if len(cleaned_name) != len(shortened_name):
-        error = "Maximum characters exceeded"
-    elif '_' not in shortened_name:
+    if special_char_removed_name != cleaned_name:
         error = "Special characters found"
+    elif len(cleaned_name) != len(shortened_name):
+        error = "Maximum characters exceeded"
     return shortened_name, error
 
+name = "Hello-123 @Test"
+shortened_name, error = cleaning_up_special_characters(name)
+
+print("Shortened Name:", shortened_name)
+print("Error:", error)

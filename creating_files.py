87b7665308ee3a4153cd86256a09d04f464c9619
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-11 17:45:12 -0400

    Update creating_files.py

--- creating_files.py
+++ creating_files.py
@@ -1,6 +1,5 @@
 import os
 import json
-import hashlib
 
 def create_test_case_files(test_cases):
     directory_path = 'test-data1'
@@ -10,24 +9,33 @@ def create_test_case_files(test_cases):
         os.makedirs(directory_path)
 
     for test_case in test_cases:
-        # Generate a hashed directory name based on the test case name
-        hash_value = hashlib.sha1(test_case.name.encode()).hexdigest()
-        subdir_name = hash_value[:2]
-        subdir_path = os.path.join(directory_path, subdir_name)
+        # Truncate the directory path and file name if necessary
+        truncated_path, truncated_name = truncate_path_and_file_name(directory_path, test_case.name, 256)
 
         # Create the subdirectory if it doesn't exist
-        if not os.path.exists(subdir_path):
-            os.makedirs(subdir_path)
-
-        # Truncate the file name if it exceeds 256 characters
-        truncated_name = test_case.name[:252] if len(test_case.name) > 256 else test_case.name
+        if not os.path.exists(truncated_path):
+            os.makedirs(truncated_path)
 
         # Create the file path within the subdirectory
-        file_name = f"{truncated_name}.json"
-        file_path = os.path.join(subdir_path, file_name)
+        file_path = os.path.join(truncated_path, f"{truncated_name}.json")
 
         # Write the test case data to the file
         with open(file_path, 'w') as f:
             json.dump(test_case.case_data, f)
 
         print(f"File created: {file_path}")
+
+
+def truncate_path_and_file_name(directory_path, file_name, max_length):
+    # Calculate the maximum allowed length for the directory path and the file name
+    max_dir_length = max_length - len(file_name) - len(os.sep) - 5  # Subtracting room for separators and extension
+
+    # Truncate the directory path if it exceeds the maximum length
+    truncated_path = directory_path[:max_dir_length]
+
+    # Truncate the file name if it exceeds the maximum length
+    truncated_name = file_name[:max_length]
+
+    # Return the truncated directory path and file name
+    return truncated_path, truncated_name
+

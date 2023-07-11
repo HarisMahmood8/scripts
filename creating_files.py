c5ea9e1ec2b8e3665d071f61110f41da80f569ad
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-11 18:20:39 -0400

    Update creating_files.py

--- creating_files.py
+++ creating_files.py
@@ -1,3 +1,7 @@
+import os
+import json
+from pathlib import Path
+
 def create_test_case_files(test_cases):
     directory_path = 'test-data1'
 
@@ -10,17 +14,16 @@ def create_test_case_files(test_cases):
         truncated_name = truncate_string(test_case.name, 256)
 
         # Create the subdirectory path by joining the directory path and truncated name
-        subdir_path = os.path.join(directory_path, truncated_name)
+        subdir_path = Path(directory_path) / truncated_name
 
         # Truncate the subdirectory path if it exceeds 256 characters
-        truncated_path = truncate_string(subdir_path, 256)
+        truncated_path = truncate_path(subdir_path, 256)
 
         # Create the subdirectory if it doesn't exist
-        if not os.path.exists(truncated_path):
-            os.makedirs(truncated_path)
+        truncated_path.mkdir(parents=True, exist_ok=True)
 
         # Create the file path within the subdirectory
-        file_path = os.path.join(truncated_path, f"{truncated_name}.json")
+        file_path = truncated_path / f"{truncated_name}.json"
 
         # Write the test case data to the file
         with open(file_path, 'w') as f:
@@ -33,3 +36,11 @@ def truncate_string(string, max_length):
     if len(string) > max_length:
         return string[:max_length]
     return string
+
+def truncate_path(path, max_length):
+    # Truncate the path components if it exceeds the maximum length
+    components = path.parts
+    truncated_components = [truncate_string(c, max_length) for c in components]
+    truncated_path = Path(*truncated_components)
+    return truncated_path
+

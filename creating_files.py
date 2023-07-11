7f49c1b5bb6ff1a15a83dd69cac0cb7762193291
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-11 17:34:57 -0400

    Update creating_files.py

--- creating_files.py
+++ creating_files.py
@@ -1,3 +1,7 @@
+import os
+import json
+import hashlib
+
 def create_test_case_files(test_cases):
     directory_path = 'test-data1'
 
@@ -6,12 +10,21 @@ def create_test_case_files(test_cases):
         os.makedirs(directory_path)
 
     for test_case in test_cases:
+        # Generate a hashed directory name based on the test case name
+        hash_value = hashlib.sha1(test_case.name.encode()).hexdigest()
+        subdir_name = hash_value[:2]
+        subdir_path = os.path.join(directory_path, subdir_name)
+
+        # Create the subdirectory if it doesn't exist
+        if not os.path.exists(subdir_path):
+            os.makedirs(subdir_path)
+
         # Truncate the file name if it exceeds 256 characters
         truncated_name = test_case.name[:252] if len(test_case.name) > 256 else test_case.name
 
-        # Create the file path
+        # Create the file path within the subdirectory
         file_name = f"{truncated_name}.json"
-        file_path = os.path.join(directory_path, file_name)
+        file_path = os.path.join(subdir_path, file_name)
 
         # Write the test case data to the file
         with open(file_path, 'w') as f:

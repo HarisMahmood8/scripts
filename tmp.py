b919e30b25559abd90dd9aa7ca64c19d9df91e5b
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-11 13:47:43 -0400

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -8,3 +8,17 @@ def cleaning_up_special_characters(name):
     elif '_' not in shortened_name:
         error = "Special characters found"
     return shortened_name, error
+
+
+def cleaning_up_special_characters(name):
+    error = None
+    max_length = 255
+    cleaned_name = name.replace(" ", "_")
+    special_char_removed_name = ''.join(c for c in cleaned_name if c.isalnum() or c == '_')
+    shortened_name = special_char_removed_name[:max_length]
+    if len(cleaned_name) != len(shortened_name):
+        error = "Maximum characters exceeded"
+    elif '_' not in shortened_name:
+        error = "Special characters found"
+    return shortened_name, error
+

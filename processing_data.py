2cab85d8df423e3327a8db8cc6df7b77a978ccc4
Haris Mahmood <haris.mahmood@uconn.edu>
2023-07-14 11:36:27 -0400

    Update processing_data.py

--- processing_data.py
+++ processing_data.py
@@ -1,4 +1,4 @@
-from Reading_files import generate_full
+from Reading_files import getAllTestCases
 import pandas as pd
 import os
 import json
@@ -7,9 +7,9 @@ import pathlib
 
 '''
 Returns a list of unique test cases that were chosen (in the case of duplicates) based on precedence.
-Returns a data frame with information on each test case
+Returns a data frame with information on each test case.
 '''
-def remove_duplicates(all_test_cases):
+def findDuplicates(all_test_cases):
     unique_test_cases = []
     duplicate_cases = []
     report_data = []
@@ -35,10 +35,11 @@ def remove_duplicates(all_test_cases):
                 
                 # if data is not the same
                 if test_case.case_data != unique_case.case_data: 
-                    test_case.folder = os.path.join("./new-data", "duplicates", test_case.folder)
+                    test_case.new_path = os.path.join("./new-data", "duplicates", test_case.new_path)
                     unique_test_cases.append(test_case)
                     test_case.duplicate = "yes*"
                 else:
+                    test_case.new_path = "DNE"
                     test_case.duplicate = "yes"
                     duplicate_cases.append(test_case) # list of duplicates with same data
     
@@ -49,9 +50,10 @@ def remove_duplicates(all_test_cases):
         if not is_duplicate:
             test_case.duplicate = "no"
             unique_test_cases.append(test_case)
-            test_case.folder = os.path.join("./new-data", test_case.folder)
-            #create_test_case_file(test_case)
-        
+            test_case.new_path = os.path.join("./new-data", test_case.new_path)
+    
+    countDuplicates(unique_test_cases + duplicate_cases) # alters the duplicatesCount attribute
+    
     #Looping through all test cases
     for test_case in unique_test_cases + duplicate_cases:
         if test_case.name in duplicate_environments:
@@ -59,16 +61,22 @@ def remove_duplicates(all_test_cases):
             environment_kept = found_environments[0]
             found_in_environments = "/".join(found_environments)
         else:
-            environment_kept = test_case.environment
+            environment_kept = None
             found_in_environments = test_case.environment
             
-        cleaning_up_name(test_case) # sets the test case name or test case modified name shorter if too long
+        if test_case.new_path != "DNE":
+            path = (os.path.join(test_case.new_path,test_case.name) + ".json").replace("\\","/")
+        else:
+            path = "DNE"
+            
+        cleaningName(test_case) # sets the test case name or test case modified name shorter if too long
         report_data.append({ #writes info for the report
             'Environment': test_case.environment,
-            'Path': test_case.new_path,
-            'Original Path': test_case.old_path,
+            'Path': path,
+            'Original Path':"./" + test_case.old_path + ".json",
             'Name': test_case.name,
             'Duplicate': test_case.duplicate,
+            'Number of Duplicates': test_case.duplicateCount,
             'Environment Kept': environment_kept,
             'Found in Environments': found_in_environments,
             'Naming Error': test_case.error
@@ -79,32 +87,25 @@ def remove_duplicates(all_test_cases):
     #creates data frame merging the three lists into their own columns
     return unique_test_cases, report_df
 
-# Shortening file paths that are too long and storing error
-def cleaning_up_name(test_case):
+# Finding special characters and shortening file paths that are too long and storing error
+def cleaningName(test_case):
     test_case.error = None
     cleaned_name = test_case.name
     spec_char_removed = ''.join(c for c in cleaned_name if c.isalnum() or c == '_')
     if spec_char_removed != cleaned_name:
         test_case.name = spec_char_removed
         test_case.error = "Special characters found"
-    if (len(os.path.abspath(test_case.folder)) + len(test_case.name) >= 251):
-        test_case.name = test_case.name[:251 - len(os.path.abspath(test_case.folder))]
+    if (len(os.path.abspath(test_case.new_path)) + len(test_case.name) >= 251):
+        test_case.name = test_case.name[:251 - len(os.path.abspath(test_case.new_path))]
         if test_case.error == None:
             test_case.error = "Maximum characters exceeded"
         else:
             test_case.error = "Special characters found / Maximum characters exceeded"   
-            
-# UNUSED CURRENTLY
-def new_name_generator(mod_name, existing_name):
-    if mod_name != existing_name: 
-        return mod_name # renaming based on environment
-    else:
-        new_name = mod_name + "_"
-        new_name_generator(new_name, existing_name)
 
-def create_test_case_files(test_cases):
+# writes test case objects to json object         
+def writeToFiles(test_cases):
     for test_case in test_cases:
-        directory_path = test_case.folder
+        directory_path = test_case.new_path
         
         if not os.path.exists(directory_path): # checks if directory path exists, if not creates it
             os.makedirs(directory_path)
@@ -114,32 +115,34 @@ def create_test_case_files(test_cases):
 
         file_path = os.path.join(directory_path, test_case.name) # create full file path
         
-        
-        try:
-            # Write the test case data to the file
-            with open(file_path, 'w') as f:
-                json.dump(test_case.case_data, f)
-            # print(f"File created: {file_path}")
-        except Exception as e:
-            print(f"Error creating file for test case '{test_case.name}': {str(e)}")
+        if not os.path.isfile(file_path):
+            try:
+                # Write the test case data to the file
+                with open(file_path, 'w') as f:
+                    json.dump(test_case.case_data, f)
+            except Exception as e:
+                print(f"Error creating file for test case '{test_case.name}': {str(e)}")
 
-
+# finds number of duplicates for each name
+def countDuplicates(test_cases):
+    file_counts = {} #create dictionary
+    for test_case in test_cases:
+        if test_case.duplicate.startswith("yes"):
+            file_counts[test_case.name] = file_counts.get(test_case.name, 0) + 1 # adds one every time it finds a duplicate
+    
+    for test_case in test_cases:
+        if test_case.name in file_counts:       
+            test_case.duplicateCount = file_counts[test_case.name] # changes duplicate count to value at name key
 
 
 if(__name__== "__main__"):
     
-    Test_Cases, df = generate_full(environment_precedence)
+    test_cases, df = getAllTestCases()
+    df.to_csv("./Files_Report.csv", index = False)
         
-    unique_cases, df = remove_duplicates(Test_Cases)
-    
-    print(f"Number of Unique Test Cases:{len(unique_cases)}")
-    df.to_csv("./Duplicates_Report.csv", index = False)
+    unique_cases, df = findDuplicates(test_cases)
+    df.to_csv("./TestCase_Report.csv", index = False)
 
-    create_test_case_files(unique_cases)
+    writeToFiles(unique_cases)
+
     
-    file_path = "new-data/"
-    paths = []
-    for root, _, files in os.walk(file_path): 
-            for file in files:
-                paths.append(os.path.join(root, file).replace("\\", "/"))
-    print(len(paths))
